73dd19cabed42db01714cf238300f90f99b3a97a
Leonardo <liogothicvampire@gmail.com>
2018-12-24 16:23:15 -0600

    Decoding Data

--- TensorFlow/TextClassificationWithMoviesReviews/main.py
+++ TensorFlow/TextClassificationWithMoviesReviews/main.py
@@ -3,6 +3,9 @@ from tensorflow import keras
 
 import numpy as np
 
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
 print(tf.__version__)
 
 imdb = keras.datasets.imdb
@@ -12,3 +15,19 @@ imdb = keras.datasets.imdb
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 print(train_data[0])
 print(len(train_data[0]), len(train_data[1]))
+
+# A dictionary mapping words to an integer index
+word_index = imdb.get_word_index()
+
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+print(decode_review(train_data[0]))
+
+
