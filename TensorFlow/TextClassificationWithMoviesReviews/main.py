70f9f39026a82c65775353e56985e059e73868fe
Leonardo <liogothicvampire@gmail.com>
2019-01-02 13:22:26 -0600

    Create a validation set

--- TensorFlow/TextClassificationWithMoviesReviews/main.py
+++ TensorFlow/TextClassificationWithMoviesReviews/main.py
@@ -69,3 +69,13 @@ model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
 model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
