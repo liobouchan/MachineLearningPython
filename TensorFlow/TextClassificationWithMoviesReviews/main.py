38b89bddd8b903a41a29405dbbb9811fec872c02
Leonardo <liogothicvampire@gmail.com>
2019-01-02 15:14:25 -0600

    Installiong matplotlib

--- TensorFlow/TextClassificationWithMoviesReviews/main.py
+++ TensorFlow/TextClassificationWithMoviesReviews/main.py
@@ -91,3 +91,26 @@ print()
 
 results = model.evaluate(test_data, test_labels)
 print(results)
+print()
+history_dict = history.history
+history_dict.keys()
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
