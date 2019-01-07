a3c68598faaaacb8c8a5b76506b9985b26cc47db
Leonardo <liogothicvampire@gmail.com>
2019-01-07 17:20:13 -0600

    Sentiment analisis

--- Google/TestingTextClassification/main.py
+++ Google/TestingTextClassification/main.py
@@ -3,16 +3,23 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import pytest
+import pandas as pd
 
 from pathlib import Path
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
 
+from tensorflow.python.keras.preprocessing import sequence
+from tensorflow.python.keras.preprocessing import text
 from tensorflow.python.keras import models
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.layers import Dropout
-
+from tensorflow.python.keras.layers import Embedding
+from tensorflow.python.keras.layers import SeparableConv1D
+from tensorflow.python.keras.layers import MaxPooling1D
+from tensorflow.python.keras.layers import GlobalAveragePooling1D
 
 # Vectorization parameters
 # Range (inclusive) of n-gram sizes for tokenizing text.
@@ -27,6 +34,12 @@ TOKEN_MODE = 'word'
 
 # Minimum document/corpus frequency below which a token will be discarded.
 MIN_DOCUMENT_FREQUENCY = 2
+
+FLAGS = None
+
+# Limit on the length of text sequences. Sequences longer than this
+# will be truncated.
+MAX_SEQUENCE_LENGTH = 500
 
 def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
     """Loads the IMDb movie reviews sentiment analysis dataset.
@@ -82,6 +95,65 @@ def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
     return ((train_texts, np.array(train_labels)),
             (test_texts, np.array(test_labels)))
 
+def _load_and_shuffle_data(data_path,
+                           file_name,
+                           cols,
+                           seed,
+                           separator=',',
+                           header=0):
+    """Loads and shuffles the dataset using pandas.
+    # Arguments
+        data_path: string, path to the data directory.
+        file_name: string, name of the data file.
+        cols: list, columns to load from the data file.
+        seed: int, seed for randomizer.
+        separator: string, separator to use for splitting data.
+        header: int, row to use as data header.
+    """
+    np.random.seed(seed)
+    data_path = os.path.join(data_path, file_name)
+    data = pd.read_csv(data_path, usecols=cols, sep=separator, header=header)
+    return data.reindex(np.random.permutation(data.index))
+
+def _split_training_and_validation_sets(texts, labels, validation_split):
+    """Splits the texts and labels into training and validation sets.
+    # Arguments
+        texts: list, text data.
+        labels: list, label data.
+        validation_split: float, percentage of data to use for validation.
+    # Returns
+        A tuple of training and validation data.
+    """
+    num_training_samples = int((1 - validation_split) * len(texts))
+    return ((texts[:num_training_samples], labels[:num_training_samples]),
+            (texts[num_training_samples:], labels[num_training_samples:]))
+
+def load_rotten_tomatoes_sentiment_analysis_dataset(data_path,
+                                                    validation_split=0.2,
+                                                    seed=123):
+    """Loads the rotten tomatoes sentiment analysis dataset.
+    # Arguments
+        data_path: string, path to the data directory.
+        validation_split: float, percentage of data to use for validation.
+        seed: int, seed for randomizer.
+    # Returns
+        A tuple of training and validation data.
+        Number of training samples: 124848
+        Number of test samples: 31212
+        Number of categories: 5 (0 - negative, 1 - somewhat negative,
+                2 - neutral, 3 - somewhat positive, 4 - positive)
+    # References
+        https://www.kaggle.com/c/sentiment-analysis-on-movie-reviews/data
+        Download and uncompress archive from:
+        https://www.kaggle.com/c/3810/download/train.tsv.zip
+    """
+    columns = (2, 3)  # 2 - Phrases, 3 - Sentiment.
+    data = _load_and_shuffle_data(data_path, 'train.tsv', columns, seed, '\t')
+
+    # Get the review phrase and sentiment values.
+    texts = list(data['Phrase'])
+    labels = np.array(data['Sentiment'])
+    return _split_training_and_validation_sets(texts, labels, validation_split)
 #For Testing
 def get_num_words_per_sample(sample_texts):
     """Returns the median number of words per sample given corpus.
@@ -145,6 +217,36 @@ def ngram_vectorize(train_texts, train_labels, val_texts):
     x_val = selector.transform(x_val).astype('float32')
     return x_train, x_val
 
+def sequence_vectorize(train_texts, val_texts):
+    """Vectorizes texts as sequence vectors.
+    1 text = 1 sequence vector with fixed length.
+    # Arguments
+        train_texts: list, training text strings.
+        val_texts: list, validation text strings.
+    # Returns
+        x_train, x_val, word_index: vectorized training and validation
+            texts and word index dictionary.
+    """
+    # Create vocabulary with training texts.
+    tokenizer = text.Tokenizer(num_words=TOP_K)
+    tokenizer.fit_on_texts(train_texts)
+
+    # Vectorize training and validation texts.
+    x_train = tokenizer.texts_to_sequences(train_texts)
+    x_val = tokenizer.texts_to_sequences(val_texts)
+
+    # Get max sequence length.
+    max_length = len(max(x_train, key=len))
+    if max_length > MAX_SEQUENCE_LENGTH:
+        max_length = MAX_SEQUENCE_LENGTH
+
+    # Fix sequence length to max value. Sequences shorter than the length are
+    # padded in the beginning and sequences longer are truncated
+    # at the beginning.
+    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
+    x_val = sequence.pad_sequences(x_val, maxlen=max_length)
+    return x_train, x_val, tokenizer.word_index
+
 def _get_last_layer_units_and_activation(num_classes):
     """Gets the # units and activation function for the last network layer.
 
@@ -184,6 +286,84 @@ def mlp_model(layers, units, dropout_rate, input_shape, num_classes):
         model.add(Dropout(rate=dropout_rate))
 
     model.add(Dense(units=op_units, activation=op_activation))
+    return model
+
+def sepcnn_model(blocks,
+                 filters,
+                 kernel_size,
+                 embedding_dim,
+                 dropout_rate,
+                 pool_size,
+                 input_shape,
+                 num_classes,
+                 num_features,
+                 use_pretrained_embedding=False,
+                 is_embedding_trainable=False,
+                 embedding_matrix=None):
+    """Creates an instance of a separable CNN model.
+    # Arguments
+        blocks: int, number of pairs of sepCNN and pooling blocks in the model.
+        filters: int, output dimension of the layers.
+        kernel_size: int, length of the convolution window.
+        embedding_dim: int, dimension of the embedding vectors.
+        dropout_rate: float, percentage of input to drop at Dropout layers.
+        pool_size: int, factor by which to downscale input at MaxPooling layer.
+        input_shape: tuple, shape of input to the model.
+        num_classes: int, number of output classes.
+        num_features: int, number of words (embedding input dimension).
+        use_pretrained_embedding: bool, true if pre-trained embedding is on.
+        is_embedding_trainable: bool, true if embedding layer is trainable.
+        embedding_matrix: dict, dictionary with embedding coefficients.
+    # Returns
+        A sepCNN model instance.
+    """
+    op_units, op_activation = _get_last_layer_units_and_activation(num_classes)
+    model = models.Sequential()
+
+    # Add embedding layer. If pre-trained embedding is used add weights to the
+    # embeddings layer and set trainable to input is_embedding_trainable flag.
+    if use_pretrained_embedding:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0],
+                            weights=[embedding_matrix],
+                            trainable=is_embedding_trainable))
+    else:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0]))
+
+    for _ in range(blocks-1):
+        model.add(Dropout(rate=dropout_rate))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(MaxPooling1D(pool_size=pool_size))
+
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Dense(op_units, activation=op_activation))
     return model
 
 def get_num_classes(labels):
@@ -292,12 +472,108 @@ def train_ngram_model(data,
     model.save('IMDb_mlp_model.h5')
     return history['val_acc'][-1], history['val_loss'][-1]
 
+def train_sequence_model(data,
+                         learning_rate=1e-3,
+                         epochs=1000,
+                         batch_size=128,
+                         blocks=2,
+                         filters=64,
+                         dropout_rate=0.2,
+                         embedding_dim=200,
+                         kernel_size=3,
+                         pool_size=3):
+    """Trains sequence model on the given dataset.
+    # Arguments
+        data: tuples of training and test texts and labels.
+        learning_rate: float, learning rate for training model.
+        epochs: int, number of epochs.
+        batch_size: int, number of samples per batch.
+        blocks: int, number of pairs of sepCNN and pooling blocks in the model.
+        filters: int, output dimension of sepCNN layers in the model.
+        dropout_rate: float: percentage of input to drop at Dropout layers.
+        embedding_dim: int, dimension of the embedding vectors.
+        kernel_size: int, length of the convolution window.
+        pool_size: int, factor by which to downscale input at MaxPooling layer.
+    # Raises
+        ValueError: If validation data has label values which were not seen
+            in the training data.
+    """
+    # Get the data.
+    (train_texts, train_labels), (val_texts, val_labels) = data
+
+    # Verify that validation labels are in the same range as training labels.
+    num_classes = get_num_classes(train_labels)
+    unexpected_labels = [v for v in val_labels if v not in range(num_classes)]
+    if len(unexpected_labels):
+        raise ValueError('Unexpected label values found in the validation set:'
+                         ' {unexpected_labels}. Please make sure that the '
+                         'labels in the validation set are in the same range '
+                         'as training labels.'.format(
+                             unexpected_labels=unexpected_labels))
+
+    # Vectorize texts.
+    x_train, x_val, word_index = sequence_vectorize(
+            train_texts, val_texts)
+
+    # Number of features will be the embedding input dimension. Add 1 for the
+    # reserved index 0.
+    num_features = min(len(word_index) + 1, TOP_K)
+
+    # Create model instance.
+    model = sepcnn_model(blocks=blocks,
+                                     filters=filters,
+                                     kernel_size=kernel_size,
+                                     embedding_dim=embedding_dim,
+                                     dropout_rate=dropout_rate,
+                                     pool_size=pool_size,
+                                     input_shape=x_train.shape[1:],
+                                     num_classes=num_classes,
+                                     num_features=num_features)
+
+    # Compile model with learning parameters.
+    if num_classes == 2:
+        loss = 'binary_crossentropy'
+    else:
+        loss = 'sparse_categorical_crossentropy'
+    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+
+    # Create callback for early stopping on validation loss. If the loss does
+    # not decrease in two consecutive tries, stop training.
+    callbacks = [tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=2)]
+
+    # Train and validate model.
+    history = model.fit(
+            x_train,
+            train_labels,
+            epochs=epochs,
+            callbacks=callbacks,
+            validation_data=(x_val, val_labels),
+            verbose=2,  # Logs once per epoch.
+            batch_size=batch_size)
+
+    # Print results.
+    history = history.history
+    print('Validation accuracy: {acc}, loss: {loss}'.format(
+            acc=history['val_acc'][-1], loss=history['val_loss'][-1]))
+
+    # Save model.
+    model.save('sepcnn_model.h5')
+    return history['val_acc'][-1], history['val_loss'][-1]
+
 #Obteniendo la Path
 mypath = Path().absolute()
 print(mypath)
 
 data = load_imdb_sentiment_analysis_dataset(mypath)
-
+acc, loss = train_ngram_model(data)
+assert acc == pytest.approx(0.91, 0.02)
+assert loss == pytest.approx(0.24, 0.02)
 # get_num_words_per_sample("Hola Pruebita")
 # plot_sample_length_distribution("Hola Pruebita")
 
+data2 = load_rotten_tomatoes_sentiment_analysis_dataset(mypath)
+acc2, loss2 = train_sequence_model(data2)
+assert acc2 == pytest.approx(0.68, 0.02)
+assert loss2 == pytest.approx(0.82, 0.02)
