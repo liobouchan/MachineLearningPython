3f72768920ba0902bddc32578ab13f836b05c3cd
Leonardo <liogothicvampire@gmail.com>
2019-01-07 12:46:13 -0600

    Adding functions ofvextorization and normalization

--- Google/TestingTextClassification/main.py
+++ Google/TestingTextClassification/main.py
@@ -2,7 +2,11 @@ import os
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+
 from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
 def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
     """Loads the IMDb movie reviews sentiment analysis dataset.
@@ -58,6 +62,7 @@ def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
     return ((train_texts, np.array(train_labels)),
             (test_texts, np.array(test_labels)))
 
+#For Testing
 def get_num_words_per_sample(sample_texts):
     """Returns the median number of words per sample given corpus.
 
@@ -68,6 +73,7 @@ def get_num_words_per_sample(sample_texts):
         int, median number of words per sample.
     """
     num_words = [len(s.split()) for s in sample_texts]
+    print(num_words)
     return np.median(num_words)
 
 def plot_sample_length_distribution(sample_texts):
@@ -82,10 +88,62 @@ def plot_sample_length_distribution(sample_texts):
     plt.title('Sample length distribution')
     plt.show()
 
+# Vectorization parameters
+# Range (inclusive) of n-gram sizes for tokenizing text.
+NGRAM_RANGE = (1, 2)
 
+# Limit on the number of features. We use the top 20K features.
+TOP_K = 20000
+
+# Whether text should be split into word or character n-grams.
+# One of 'word', 'char'.
+TOKEN_MODE = 'word'
+
+# Minimum document/corpus frequency below which a token will be discarded.
+MIN_DOCUMENT_FREQUENCY = 2
+
+def ngram_vectorize(train_texts, train_labels, val_texts):
+    """Vectorizes texts as n-gram vectors.
+
+    1 text = 1 tf-idf vector the length of vocabulary of unigrams + bigrams.
+
+    # Arguments
+        train_texts: list, training text strings.
+        train_labels: np.ndarray, training labels.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val: vectorized training and validation texts
+    """
+    # Create keyword arguments to pass to the 'tf-idf' vectorizer.
+    kwargs = {
+            'ngram_range': NGRAM_RANGE,  # Use 1-grams + 2-grams.
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': TOKEN_MODE,  # Split text into word tokens.
+            'min_df': MIN_DOCUMENT_FREQUENCY,
+    }
+    vectorizer = TfidfVectorizer(**kwargs)
+
+    # Learn vocabulary from training texts and vectorize training texts.
+    x_train = vectorizer.fit_transform(train_texts)
+
+    # Vectorize validation texts.
+    x_val = vectorizer.transform(val_texts)
+
+    # Select top 'k' of the vectorized features.
+    selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
+    selector.fit(x_train, train_labels)
+    x_train = selector.transform(x_train).astype('float32')
+    x_val = selector.transform(x_val).astype('float32')
+    return x_train, x_val
+
+#Obteniendo la Path
 mypath = Path().absolute()
 print(mypath)
 
 load_imdb_sentiment_analysis_dataset(mypath)
-get_num_words_per_sample("Hola Pruebita")
-plot_sample_length_distribution("Hola Pruebita")
+# get_num_words_per_sample("Hola Pruebita")
+# plot_sample_length_distribution("Hola Pruebita")
+
