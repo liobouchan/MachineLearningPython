c4a0ca9447b94de7b1154b1e33b3d2a54585d2d8
Leonardo <liogothicvampire@gmail.com>
2019-01-07 13:54:19 -0600

    add the get_enum_classes fuction

--- Google/TestingTextClassification/main.py
+++ Google/TestingTextClassification/main.py
@@ -183,6 +183,34 @@ def mlp_model(layers, units, dropout_rate, input_shape, num_classes):
     model.add(Dense(units=op_units, activation=op_activation))
     return model
 
+def get_num_classes(labels):
+    """Gets the total number of classes.
+    # Arguments
+        labels: list, label values.
+            There should be at lease one sample for values in the
+            range (0, num_classes -1)
+    # Returns
+        int, total number of classes.
+    # Raises
+        ValueError: if any label value in the range(0, num_classes - 1)
+            is missing or if number of classes is <= 1.
+    """
+    num_classes = max(labels) + 1
+    missing_classes = [i for i in range(num_classes) if i not in labels]
+    if len(missing_classes):
+        raise ValueError('Missing samples with label value(s) '
+                         '{missing_classes}. Please make sure you have '
+                         'at least one sample for every label value '
+                         'in the range(0, {max_class})'.format(
+                            missing_classes=missing_classes,
+                            max_class=num_classes - 1))
+
+    if num_classes <= 1:
+        raise ValueError('Invalid number of labels: {num_classes}.'
+                         'Please make sure there are at least two classes '
+                         'of samples'.format(num_classes=num_classes))
+    return num_classes
+
 def train_ngram_model(data,
                       learning_rate=1e-3,
                       epochs=1000,
@@ -209,7 +237,7 @@ def train_ngram_model(data,
     (train_texts, train_labels), (val_texts, val_labels) = data
 
     # Verify that validation labels are in the same range as training labels.
-    num_classes = explore_data.get_num_classes(train_labels)
+    num_classes = get_num_classes(train_labels)
     unexpected_labels = [v for v in val_labels if v not in range(num_classes)]
     if len(unexpected_labels):
         raise ValueError('Unexpected label values found in the validation set:'
