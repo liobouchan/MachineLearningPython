e714e9c1193c6a36dab0e08d5c0fdf1b62cdb7a8
Leonardo <liogothicvampire@gmail.com>
2019-01-18 14:41:55 -0600

    Downloading data from a external CSV and save to a Data fRame

--- Google/IntroPandas/intro.py
+++ Google/IntroPandas/intro.py
@@ -11,3 +11,7 @@ print("\n Print a Series : \n " , pd.Series(['San Francisco', 'San Jose', 'Sacra
 city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
 population = pd.Series([852469, 1015785, 485199])
 print("\n Print a DataFrame: \n " , pd.DataFrame({ 'City name': city_names, 'Population': population }))
+
+#Load an entire file into a DataFrame
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+print("\n Print data downloaded CSV : \n" , california_housing_dataframe.describe())
