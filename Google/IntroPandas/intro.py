81f4b7ffcfcd29ffa63aadf108101b3ce2b1e943
Leonardo <liogothicvampire@gmail.com>
2019-01-18 14:24:45 -0600

    Printing a Series and a DataFrame

--- Google/IntroPandas/intro.py
+++ Google/IntroPandas/intro.py
@@ -2,4 +2,12 @@ from __future__ import print_function
 
 import pandas as pd
 
-print("Print pandas version " , pd.__version__)
+print("\nPrint pandas version " , pd.__version__)
+
+#Create a Series Object
+print("\n Print a Series : \n " , pd.Series(['San Francisco', 'San Jose', 'Sacramento']))
+
+#DataFrame objects can be created by passing a dict mapping string column names to their respective Series.
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+print("\n Print a DataFrame: \n " , pd.DataFrame({ 'City name': city_names, 'Population': population }))
